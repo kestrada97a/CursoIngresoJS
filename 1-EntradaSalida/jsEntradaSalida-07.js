c180e4c2e6b6aa7db61f20bc798b5f2d95d5933c
brake <estrada.meza.kevin@gmail.com>
2021-07-07 15:29:58 -0300

    ejercicios hechos

--- 1-EntradaSalida/jsEntradaSalida-07.js
+++ 1-EntradaSalida/jsEntradaSalida-07.js
@@ -3,23 +3,40 @@ Debemos lograr tomar Los numeros por ID ,
 transformarlos a enteros (parseInt),realizar la operación correcta y 
 mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
+
+
+
+
+
 function sumar()
 {	
-	alert("ok");	
+	let uno = parseInt(document.getElementById("txtIdNumeroUno").value);
+    let dos = parseInt(document.getElementById("txtIdNumeroDos").value);
+	let suma = uno + dos;
+	alert("el resultado es " + suma);	
 }
 
 function restar()
 {
-	
+	let uno = parseInt(document.getElementById("txtIdNumeroUno").value);
+    let dos = parseInt(document.getElementById("txtIdNumeroDos").value);
+	let resta = uno - dos;
+	alert("el resultado es " + resta); 
 }
 
 function multiplicar()
 { 
-	
+	let uno = parseInt(document.getElementById("txtIdNumeroUno").value);
+    let dos = parseInt(document.getElementById("txtIdNumeroDos").value);
+	let mult = uno * dos;
+	alert("el resultado es " + mult);
 }
 
 function dividir()
 {
-	
+	let uno = parseInt(document.getElementById("txtIdNumeroUno").value);
+    let dos = parseInt(document.getElementById("txtIdNumeroDos").value);
+	let div = uno / dos;
+	alert("el resultado es " + div);
 }
 
